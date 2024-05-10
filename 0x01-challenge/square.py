df7420ee9197daa9a29d551e741eb74addea9ecc
Victory Chibuike <victorychibuike121@gmail.com>
2024-05-10 15:59:25 +0100

    Restored the code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
 
+"""
+Defines a square clsss
+Not a rectangle
+or a cuboid
+
+"""
 class square():
+    """ The real square class """
   
 	width = 0
 	height = 0
 
 
 	def __init__(self, *args, **kwargs):
+        """ Initialization function """
+
 		for key, value in kwargs.items():
 			setattr(self, key, value)
 
 	def area_of_my_square(self):
 		""" Area of the square """
+
 		return self.width * self.width
 
 	def PermiterOfMySquare(self):
+        """ Perimeter of square """
+
 		return (self.width * 2) + (self.height * 2)
 
 	def __str__(self):
+        """ String representation of square """
+
 		return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
