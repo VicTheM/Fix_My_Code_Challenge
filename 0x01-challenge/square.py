57b5c58c8c69694d812d82e8cab44aec0e24aa59
Victory <victorychibuike121@gmail.com>
2024-05-16 02:17:43 +0100

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,42 +1,33 @@
 #!/usr/bin/python3
-
-"""
-Defines a square clsss
-Not a rectangle
-or a cuboid
-
-"""
-class square():
-    """ The real square class """
-  
-	width = 0
-	height = 0
+""" Module for square class"""
 
 
-	def __init__(self, *args, **kwargs):
-        """ Initialization function """
+class Square():
+    """ Square class """
+    width = 0
+    height = 0
 
-		for key, value in kwargs.items():
-			setattr(self, key, value)
+    def __init__(self, *args, **kwargs):
+        """ Instantiation of class """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-	def area_of_my_square(self):
-		""" Area of the square """
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
 
-		return self.width * self.width
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
+        return (self.width * 2) + (self.height * 2)
 
-	def PermiterOfMySquare(self):
-        """ Perimeter of square """
+    def __str__(self):
+        """ Printable representation """
+        return "{}/{}".format(self.width, self.height)
 
-		return (self.width * 2) + (self.height * 2)
-
-	def __str__(self):
-        """ String representation of square """
-
-		return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-	s = square(width=12, height=9)
-	print(s)
-	print(s.area_of_my_square())
-	print(s.PermiterOfMySquare())isr/bin/python3
+    """ Create a square object """
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
